0b3e2cb2f22b14e919b458e8cc3c60b274cb1432
johanahlavacova3-hue <johana.hlavacova3@gmail.com>
2025-11-25 11:30:19 +0100

    Update script.js

--- POUTNICI/script.js
+++ POUTNICI/script.js
@@ -1,19 +1,15 @@
 // script.js
 
-// Najdeme náš model na stránce
 const modelViewer = document.querySelector('model-viewer');
 
-// Zkontrolujeme, jestli existuje
 if (modelViewer) {
     
-    // Můžeme například poslouchat událost 'load',
-    // která se spustí, až bude model plně načten.
     modelViewer.addEventListener('load', () => {
         console.log('Model CHATA.glb byl úspěšně načten!');
     });
 
-    // Pro samotné otáčení a ovládání myší není potřeba žádný JS kód,
-    // vše je již nastaveno v HTML (auto-rotate a camera-controls).
+    // Všechno (otáčení, responzivita) je řízeno 
+    // HTML atributy a CSS souborem.
     
 } else {
     console.error('Element <model-viewer> nebyl na stránce nalezen.');
