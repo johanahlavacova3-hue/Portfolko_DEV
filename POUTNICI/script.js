27a944868b24d983466c9c5c58168ef75eda733d
johanahlavacova3-hue <johana.hlavacova3@gmail.com>
2025-11-25 11:51:00 +0100

    Update script.js

--- POUTNICI/script.js
+++ POUTNICI/script.js
@@ -7,9 +7,6 @@ if (modelViewer) {
     modelViewer.addEventListener('load', () => {
         console.log('Model CHATA.glb byl úspěšně načten!');
     });
-
-    // Všechno (otáčení, responzivita) je řízeno 
-    // HTML atributy a CSS souborem.
     
 } else {
     console.error('Element <model-viewer> nebyl na stránce nalezen.');
